bd1e50cf57b7a2882174c0eeb45b780fcca347fb
Kai <bott0r@users.noreply.github.com>
2017-04-26 15:43:33 +0200

    Update 98_check_db_access.sh

--- init_scripts/98_check_db_access.sh
+++ init_scripts/98_check_db_access.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+
+sleep 60s 
 echo "Checking can connect to mongo and redis"
 cd /var/www/sharelatex && grunt check:redis
+sleep 60s & echo "waiting 60s"
 cd /var/www/sharelatex && grunt check:mongo
 echo "All checks passed"
